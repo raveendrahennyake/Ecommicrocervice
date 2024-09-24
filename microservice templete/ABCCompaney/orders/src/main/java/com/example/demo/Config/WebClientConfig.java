39a0224c8f0464aa9d919e0bf4d054f7b20c5175
Raveendra Hennyake <raveendralakshan5@gmail.com>
2024-09-24 20:52:43 +0530

    implementmultipleinstance

--- microservice templete/ABCCompaney/orders/src/main/java/com/example/demo/Config/WebClientConfig.java
+++ microservice templete/ABCCompaney/orders/src/main/java/com/example/demo/Config/WebClientConfig.java
@@ -1,22 +1,24 @@
 package com.example.demo.Config;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     @Bean
-    public WebClient webClient (){
-        return WebClient.builder().build();
+    @LoadBalanced
+    public WebClient.Builder WebClientBuilder (){
+        return WebClient.builder();
     }
     @Bean
     @Qualifier("inventoryWebClient")
     public WebClient inventoryWebClient (){
-        return WebClient.builder().baseUrl("http://localhost:8081/api/v1").build();
+        return WebClientBuilder().baseUrl("http://inventory/api/v1").build();
     }
     @Bean
     @Qualifier("ProductWebClient")
     public WebClient ProductWebClient (){
-        return WebClient.builder().baseUrl("http://localhost:8082/api/v1").build();
+        return WebClientBuilder().baseUrl("http://product/api/v1").build();
     }
 }
